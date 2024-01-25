ff07dac678fa0666114fa08bcf5998ae6ed57203
Himanshu Yadav <himanshuyadav2365@gmail.com>
2024-01-25 17:16:03 +0530

    fixhealth

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-1/2 object-cover h-1/2 w-screen '>
+        <img alt="hero" className='object-cover w-screen '
+        src="https://images.squarespace-cdn.com/content/v1/61afd43e933ad92c6928b093/d60ccb2f-c821-4456-aa0f-b3b71adebe39/chipperfield-physiotherapy-home-hero.jpg"/>
+    </div>
+  )
+}
+
+export default Hero
